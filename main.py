305d2de469d43956967712e57edd6c45304b1926
TheSpace <78466469+TheSpace-hub@users.noreply.github.com>
2024-11-11 10:33:46 +0300

    LED is blinking

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import RPi.GPIO as gpio
+import time
+
+
 def main():
-    print("Hello")
+    n = 2
+    gpio.setmode(gpio.BCM)
+    gpio.setup(n, gpio.OUT, initial=gpio.LOW)
+    while True:
+        gpio.output(n, gpio.HIGH)
+        time.sleep(1)
+        gpio.output(n, gpio.LOW)
+        time.sleep(1)
 
 
 
